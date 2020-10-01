da17d37af011e9000181308aab883e282efd283e
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-01 11:20:35 +0530

    created index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/index.css">
+    <title>Euro Flora</title>
+</head>
+<body>
+<div class="navBar">
+    <a href="">Chat</a>
+    <a href="">Order by phone</a>
+    <a href="">How to order</a>
+    <a href="">Gallery</a>
+    <a href="">The florist</a>
+    <a href="">Contact us</a>
+    <a href="index.php">Home</a>
+</div>
+    
+</body>
+</html>
