0647b4fa723d5aa71101636b693105d4f3396bb6
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2020-10-06 02:04:20 +0530

    added a form to chat.php

--- chat.php
+++ chat.php
@@ -7,7 +7,22 @@
 </head>
 <body>
 
-    <h1>hello this is chat.currently workin on this</h1>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><input type="text" name="name" id=""></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="text" name="email" id=""></td>
+            </tr>
+            <tr>
+                <td>Your message:</td>
+                <td><textarea name="message" id="" cols="30" rows="10"></textarea></td>
+            </tr>
+        </table>
+    </form>
     
 </body>
 </html>
